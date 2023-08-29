868c77624d1266d62656c29a96519f93c25e513f
nihal tekin <nihalpolattekin@gmail.com>
2023-08-29 10:33:34 +0200

    forms

--- templateApp/urls.py
+++ templateApp/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path, include
-from .views import home, body, studentView
+from .views import home, body, studentView, student_addView
 
 urlpatterns = [
     path('tmp', body), 
     path('home', home),
     path('student', studentView),
+    #forms
+    path('add', student_addView),
   
 ]
