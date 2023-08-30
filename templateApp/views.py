9e4f022bc8c4b036ef76b3d974ca44ffb5cb3e9f
nihal tekin <nihalpolattekin@gmail.com>
2023-08-30 16:05:47 +0200

    aa

--- templateApp/views.py
+++ templateApp/views.py
@@ -2,40 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 # def home(request):
-#     return HttpResponse('<h1>Welcome to django template</h1>')
+#     return HttpResponse('<h1>welcome to django template</h1>')
 def home(request):
-    context={
-                     }
-   
-    return render(request, 'templateApp/home.html',context)
+     context={         
+          }             
+     return render(request,'templateApp/home.html',context)
 
 def body(request):
-    context={'title':'clarusway',
-             'path':'FS',
-             'list':['yunus', 'özlem','fatih','esra','hüseyin','nihal','ermihan','halit','irfan'],
-             'dict':{'k1':'value1',
-                     'k2':'value2'
-                     },
-                      'number':0,
-                      'desc':'this template from APP dir'
-                     }
-   
-    return render(request, 'templateApp/index.html',context)
-    # return render(request, 'templateApp/index.html',{'name':'yunus'})
-
+     context={
+          'title':'clarusway',
+          'path':'FS',
+          'list':['yunus','ozlem','fatih','esra','huseyin','nihal','emirhan','halit','irfan'],
+          'dict':{
+               'k1':'value1',
+               'k2':'value2'
+          },
+          'number':0,
+          'desc':'this template from APP dir'
+          }
+         
+     
+     return render(request,'templateApp/index.html',context)
+    # return render(request,'templateApp/index.html',{'name':'yunus'})
 from .models import Student
+
 def studentView(request):
      students=Student.objects.all()
-     context={
-         'students':students,
-          }
+     context={ 
+          'students':students,
+          }             
      return render(request,'templateApp/student.html',context)
 
-
-# Forms lessons
-
+# Froms lessons
 from .forms import StudentForm
 from django.shortcuts import redirect
 from django.views import generic
@@ -43,42 +42,42 @@ from django.urls import reverse_lazy
 
 def student_addView(request):
 
-    form=StudentForm()
-    if request.method=='POST':
-        form=StudentForm(request.POST)
-        if form.is_valid():
-             form.save()
-             return redirect('/student')
-    
-    context={
-         'form':form,
-          }
-    return render(request,'templateApp/student_add.html',context)
+     form=StudentForm()
+
+     if request.method=='POST':
+          form=StudentForm(request.POST)
+          if form.is_valid():
+               form.save()
+               return redirect ('/student')
+     context={ 
+          'form':form
+          }  
+     return render(request,'templateApp/student_add.html',context)
 
 
 class StudentAddView(generic.CreateView):
-    model=Student
-    form_class=StudentForm
-    success_url=reverse_lazy('list2')
-    # normalde modelismi_form.html diósyayi istiyor bizim dosyamizin adi student_add.html idi. biz de default olarak onu kabul etmesi icin template_app yazdik.
-    template_name='templateApp/student_add.html'
+     model=Student
+     form_class=StudentForm
+     success_url=reverse_lazy('list')
+     # default olarak modelname_form.html değiştirecekseniz templatename e  ata
+     template_name='templateApp/student_add.html'
+
 
 class StudentListView(generic.ListView):
-    model=Student
-    #pk_url_kwarg='id   pk yerine farkli kullanmak istersek
-    # form_class=StudentForm
-    # success_url=reverse_lazy('list')
-    # normalde modelismi_form.html dosyayi istiyor bizim dosyamizin adi student_add.html idi. biz de default olarak onu kabul etmesi icin template_app yazdik.
-     
-    template_name='templateApp/student.html'
+     model=Student
+     # form_class=StudentForm
+     # success_url=reverse_lazy('list')
+     # default olarak student_list.html değiştirecekseniz templatename e  ata
+     # template_name='templateApp/student.html'
 
 class StudentDetailView(generic.DetailView):
-    model=Student
-
+     model=Student
+     # pk_url_kwarg='id' # pk yerine farklı kullanmak isteseniz
+     # default olarak student_detail.html değiştirecekseniz templatename e  ata
 
 class StudentUpdateView(generic.UpdateView):
-    model=Student
-    form_class=StudentForm
-    success_url=reverse_lazy('list2')
-    # normalde modelismi_form.html diósyayi istiyor bizim dosyamizin adi student_add.html idi. biz de default olarak onu kabul etmesi icin template_app yazdik.
-    template_name='templateApp/student_add.html'
+     model=Student
+     form_class=StudentForm        
+     success_url=reverse_lazy('list')
+     # default olarak modelname_form.html değiştirecekseniz templatename e  ata
+     template_name='templateApp/student_add.html'
